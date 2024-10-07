817ded3fdecf93c5963ee0dcf1d927552ab292b0
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-10-07 06:53:22 -0500

    enabled cors all

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(7000);
 }
 bootstrap();
