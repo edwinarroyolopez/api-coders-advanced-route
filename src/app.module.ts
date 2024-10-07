25e5606dd5a82c3ea7a9d42322faa47878476285
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-10-07 07:54:40 -0500

    add two connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ dotenv.config();
 
 console.log(process.env.MONGODB_URI)
 
+
+const dbLocal = "mongodb://testperformance:abcd.1234@localhost:27017";
+const dbServer = "mongodb+srv://zeroedprogrammer:FrnnZSzyL9csqfCy@dev-test-riwi.n35bp.mongodb.net/?retryWrites=true&w=majority&appName=dev-test-riwi";
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://zeroedprogrammer:FrnnZSzyL9csqfCy@dev-test-riwi.n35bp.mongodb.net/?retryWrites=true&w=majority&appName=dev-test-riwi'),
+    MongooseModule.forRoot(dbServer),
     AuthModule,
   ],
   controllers: [],
